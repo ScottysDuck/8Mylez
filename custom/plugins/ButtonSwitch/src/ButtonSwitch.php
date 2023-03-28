e41ae165098004a41d02475b7f002d52914d57f1
ScottyDuck <larsstrunk01@hotmail.de>
2023-03-28 09:33:55 +0200

    buttonSwitch

---
+++ custom/plugins/ButtonSwitch/src/ButtonSwitch.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ButtonSwitch;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ButtonSwitch extends Plugin implements ThemeInterface
+{
+}
