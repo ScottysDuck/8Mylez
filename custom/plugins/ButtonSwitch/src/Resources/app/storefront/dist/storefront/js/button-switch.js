cb21fe0a7b47741e17d2e695180d3a15cce74817
ScottysDuck <Larsstrunk01@hotmail.de>
2023-04-09 18:27:45 +0200

    btn color switch for 1 sek

--- custom/plugins/ButtonSwitch/src/Resources/app/storefront/dist/storefront/js/button-switch.js
+++ custom/plugins/ButtonSwitch/src/Resources/app/storefront/dist/storefront/js/button-switch.js
@@ -4,11 +4,16 @@
 
 let button= document.querySelector('btn-buy');
 
+
+
 $( document ).ready(function() {
     $( ".btn-buy" ).click(function() {
-      $( ".btn-buy" ).css('background', 'grey');
-       setTimeout(function() {
-        $('.btn-buy').css('background', '#008490');
+      $( ".btn-buy:focus" ).css('background', '#625f5f');
+
+        
+        setTimeout(function() {
+        
+          $('.btn-buy').css('background', '#008490');
        }, 1000); 
     });
 });
