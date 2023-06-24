4fa5f05af81db966e1f55d7854a03f27cbc04718
ScottysDuck <Larsstrunk01@hotmail.de>
2023-06-24 16:12:25 +0200

    button Text switch

--- custom/plugins/ButtonSwitch/src/Resources/app/storefront/dist/storefront/js/button-switch.js
+++ custom/plugins/ButtonSwitch/src/Resources/app/storefront/dist/storefront/js/button-switch.js
@@ -7,10 +7,17 @@ $( document ).ready(function() {
     $( ".btn-buy" ).click(function() {
       $( ".btn-buy:focus" ).css('background', '#625f5f');
       
+      const buttonElement = document.getElementsByClassName("btn btn-block btn-buy")
+      for(const button in buttonElement) {
+        //hier muss noch ein funktionierendes if rein!!!
+        if(buttonElement.click){
+        buttonElement[button].innerText = "Will be added to cart"}}
+      
       
       setTimeout(function() {
       $('.btn-buy').css('background', '#008490');
-
+      for(const button in buttonElement) {
+        buttonElement[button].innerText = "Add to shopping cart"}
        }, 1000); 
     });
 });
